6471c2520b6827225b08a2977fc9dcf79594e09d
JillianForsyth <jillian.forsyth@mail.mcgill.ca>
2025-11-23 12:39:15 -0500

    second draft - new pages

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const navLinkEls = document.querySelectorAll('.nav_link');
+const sectionEls = document.querySelectorAll('.section');
+
+let currentSection = 'HOME';
+window.addEventListener('scroll', () => {
+    sectionEls.forEach(sectionEL => {
+        if (window.scrollY >= sectionEL.offsetTop - sectionEL.clientHeight / 3) {
+            currentSection = sectionEL.id;
+        }
+    });
+
+navLinkEls.forEach(navLinkEls => {
+    if (navLinkEl.href.includes(currentSection)) {
+        document.querySelector('.active').classList.remove('active');
+        navLinkEl.classlist.add('active');
+    }
+});
+});
